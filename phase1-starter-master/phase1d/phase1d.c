b84095c8923829c66348607ac96b8b4cee5c018a
James M Brechtel <46606094+brechtelj1@users.noreply.github.com>
2021-02-27 16:13:15 -0700

    worked on Join and Handler
    possibly finished join

--- phase1-starter-master/phase1d/phase1d.c
+++ phase1-starter-master/phase1d/phase1d.c
@@ -16,7 +16,12 @@ static void IllegalInstructionHandler(int type, void *arg);
 
 static int Sentinel(void *arg);
 
-int RUPT_COND = 0;                   // condition variable
+// condition variables
+int RUPT_COND = 0;              // interrupt condition
+
+// state variables
+static int SHOULD_WAIT = 0;     // wait state var
+static int RUPT_OCCUR = 0;      // records num interrupts 
 
 void startup(int argc, char **argv){
     int pid;
@@ -57,6 +62,7 @@ int P1_DeviceWait(int type, int unit, int *status) {
     // wait for interrupt or abort on type:unit
     while(RUPT_COND != 1){
         // wait
+        
 
         // abort
 
@@ -65,7 +71,7 @@ int P1_DeviceWait(int type, int unit, int *status) {
     if(aborted == 0){
         //  set *status to device's status
         // NOTE: Interrupt handler should call USLOSS_DeviceInput to get status
-        
+
         *status = type
     }
     // release lock
@@ -75,16 +81,50 @@ int P1_DeviceWait(int type, int unit, int *status) {
     return result;
 }
 
-
+// handles devices, type unit, etc. calls naked signal to wake up
+// the related device
 static void DeviceHandler(int type, void *arg) {
-    // record that interrupt occurred
-    // save device status
-    // if clock device
-    //      naked signal clock every 5 interrupts
-    //      P1Dispatch(TRUE) every 4 interrupts
-    // else
-    //      naked signal type:unit
+    int wakeupClock = 5;
+    int wakeupHanlder = 4;
+    static int clockRupt = 0;                      
+    // maybe TODO record that interrupt occurred
+    RUPT_OCCUR++;
+
+    // TODO save device status
+
+
+    // if clock device   NOTE: completely unsure what this is
+    if(USLOSS_CLOCK_DEV){
+        // naked signal clock every 5 interrupts
+
+        // wake up handler every 4 ticks or 80 milliseconds
+        if(clockRupt == wakeupHanlder){
+            P1Dispatch(TRUE);
+        }
+        // wake up device every 5 ticks or 100 milliseconds
+        if(clockRupt == wakeupHanlder){
+
+        }
+        
+    }
+    // else naked signal type:unit    (Types & units listed in USLOSS.h)
+    else{
+        // NOTE: Conflated naming convention on type, unsure usage
+        if(type == USLOSS_CLOCK_DEV){
+            P1_NakedSignal(USLOSS_CLOCK_DEV:1);
+        }
+        if(type == USLOSS_ALARM_DEV){
+            P1_NakedSignal(USLOSS_ALARM_DEV:1);
+        }
+        if(type == USLOSS_DISK_DEV){
+            P1_NakedSignal(USLOSS_DISK_DEV:2);
+        }
+        if(type == USLOSS_TERM_DEV){
+            P1_NakedSignal(USLOSS_TERM_DEV:4);
+        }
+    }
 }
+
 
 static int Sentinel (void *notused){
     int     pid;
@@ -101,27 +141,43 @@ static int Sentinel (void *notused){
     return 0;
 } /* End of sentinel */
 
+
+// similar to P1GetChildStatus
+// returns processid and status of quit child
+// waits for a child to quit before doing anything
 int P1_Join(int *pid, int *status) {
     int result = P1_SUCCESS;
+    int loopCond = 0;
     // disable interrupts
     P1DisableInterrupts();
-
     // check kernel mode
     CHECKKERNEL();
+    // repeat until either a child quit or there are no children
+    while(loopCond == 0){
+        // child pid & status returned in params
+        P1GetChildStatus(*pid,*status);
 
-    // repeat
-    //     call P1GetChildStatus  
-    //     if there are children but no children have quit
-    //        P1SetState(P1_STATE_JOINING)
-    //        P1Dispatch(FALSE)
-    // until either a child quit or there are no children
-    // restore interrupts
+        // check for children, if none return none
+        if(status == P1_NO_CHILDREN){
+            result = P1_NO_CHILDREN;
+            loopCond = 1;
+        }
+        // if there are children but no children have quit
+        if(status == P1_NO_QUIT){
+            P1SetState(P1_STATE_JOINING);
+            P1Dispatch(FALSE);
+        }
+        // check if a child has quit
+        if(status == P1_SUCCESS){
+            loopCond = 1;
+        }
+    }
+    // enable interrupts
     P1EnableInterrupts();
     return result;
 }
 
-static void
-SyscallHandler(int type, void *arg) {
+static void SyscallHandler(int type, void *arg) {
     USLOSS_Sysargs *sysargs = (USLOSS_Sysargs *) arg;
     USLOSS_Console("System call %d not implemented.\n", sysargs->number);
     P1_Quit(1025);
